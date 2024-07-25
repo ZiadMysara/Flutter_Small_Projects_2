b7eb419bc66fd7f3087e47f59dc1cdb3cf1e525d
Ziad Mysara <ziadmysara@gmail.com>
2024-07-25 12:44:38 +0300

    finish

--- business_card_app/lib/main.dart
+++ business_card_app/lib/main.dart
@@ -26,30 +26,64 @@ class BusinessCardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(48, 69, 88, 1),
+      backgroundColor: const Color.fromRGBO(48, 69, 88, 1),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const CircleAvatar(
               backgroundColor: Colors.white,
-              radius: 102,
+              radius: 71,
               child: CircleAvatar(
                 backgroundImage: AssetImage('assets/images/tharwat.png'),
-                radius: 100,
+                radius: 70,
               ),
             ),
-            Text(
+            const Text(
               "Tharwat Samy",
-              style: TextStyle(color: Colors.white,
-              fontFamily: 
-              ),
-
+              style: TextStyle(
+                  color: Colors.white, fontSize: 22, fontFamily: "Pacifico"),
             ),
             Text(
               'Flutter Developer'.toUpperCase(),
-              style:
-                  const TextStyle(color: Colors.white60, fontWeight: FontWeight.bold,),
+              style: const TextStyle(
+                  color: Colors.white60,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 11),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(13.0),
+              child: Container(
+                height: 50,
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(12))),
+                child: const Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 15, right: 30),
+                      child: Icon(
+                        Icons.phone,
+                        color: Color.fromRGBO(48, 69, 88, 1),
+                      ),
+                    ),
+                    Text("(+01)204995784", style: TextStyle(fontSize: 18),),
+                  ],
+                ),
+              ),
+            ),
+            const Card(
+              margin: EdgeInsets.all(13),
+              color: Colors.white,
+
+              child: ListTile(
+                iconColor: Color.fromRGBO(48, 69, 88, 1),
+                leading: Padding(
+                  padding: EdgeInsets.only( right: 20),
+                  child: Icon(Icons.email),
+                ),
+                title: Text("ScholarTech@gmail.com"),
+              ),
             )
           ],
         ),
